661e36a6ddc3e176d4c71210bcbec90df1f2a059
Mgunadi <mgun_27@hotmail.com>
2018-09-24 20:07:26 +1000

    Added content class

--- Content.java
+++ Content.java
@@ -1,15 +1,28 @@
-
+import java.util.HashMap;
 public class Content {
 	
-	String ID;
-	String appName, reviews;
-	int downloads;
-	double price;
+	private String ID, appName, reviews;
+	private int downloads;
+	private double price;
+	private HashMap<String, String> Reviews = new HashMap<String, String>();
 	
-	//Need to add in constructors
+	public Content(String ID, String appName, String reviews, int downloads, double price) {
+		this.ID = ID;
+		this.appName = appName;
+		this.reviews = reviews;
+		this.downloads = downloads;
+		this.price = price;
+	}
+	public void addComment(Comment r) {
+		Reviews.put(r.get(0));
+		
+	}
 	
-	
-	//Add in methods
+	public String showReview(String c) {
+		for (int i = 0 ; i <Reviews.size(); i++) {
+			return Reviews.get(i).get(key);  // gets the value, username, for the key, userid (eg. u1), 
+		}
+	}
 
 }
 
