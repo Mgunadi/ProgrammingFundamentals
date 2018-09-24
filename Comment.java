d8c9def72d8beb1e8a497b9b23ba94da54305bb1
Mgunadi <mgun_27@hotmail.com>
2018-09-24 20:21:22 +1000

    add Comment class

---
+++ Comment.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class Comment {
+
+	private User user;
+	private String cmt;
+	private ArrayList<Comment> Collection = new ArrayList<Comment>();
+	
+	public Comment(User u, String cmt, ArrayList<Comment> clt) {
+		this.user = u;
+		this.cmt = cmt;
+		Collection = clt;
+	}
+	
+	public void addReply(Comment com) {
+		this.Collection.add(com);
+	}
+	
+}
