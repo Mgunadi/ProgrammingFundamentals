ee900940d4e73b16efea03d75cbc6cd858ced309
VerityMiles <verityfmiles@gmail.com>
2018-09-28 14:39:52 +1000

    Work on the user class method

--- User.java
+++ User.java
@@ -16,26 +16,52 @@ public class User {
 		OSType = os;
 	}
 	
+	public boolean enoughBalance(double amount) {
+		boolean hasEnough;
+		if ((balance -= amount) > 0) {
+			System.out.println("Congratulations you have enough money!!!");
+			hasEnough = true;
+			return hasEnough;}
+		else {
+			System.out.println("You do not have enough money in your account");
+			hasEnough = false;
+			return hasEnough;}	
+	}
+	
 	public void becomePremium() {
-		if ((balance -= 100) > 0){
-			balance = balance -= 100;
-			isPremium = true;}
+		if (enoughBalance(100) == true) {
+			balance -= 100;
+			System.out.println("Your new balance is: $" + balance);
+			isPremium = true;
+			System.out.println("Congratulations you are now a premium member");
+		}
+		
 		else {
 			System.out.println("You do not have enough in your account");
+			System.out.println("Please add more money into your account and then try again");
 			isPremium = false;}			
-		
 	}
 	
 	public void buyContent(Content content) {
-		// if-else (if Premium:) ....
-		// balance - content.getPrice();
-		// content.getDownloads() + 1;
-		/// if-else (check for OS compatibility)
-	}
+		double price = content.getPrice();
+		if (Premium = true)
+			price = content.getPrice() - (content.getPrice() * 0.1); 
+		else price = price;
+		if (enoughBalance(price) == true) {
+			//buy
+			balance =- price;
+			//content.getDownloads() + 1;
+			Library.add(content);
+		}
+		else {
+			System.out.println("You do not have enough in your account");
+			//not allowed to buy
+		}
 	
 	public void showcontentBought(){
-		
-		System.out.println();
+		for (int i = 0; i < Library.size(); i++)
+			System.out.println(Library.get(i));
+	}
 		
 	}
 }
