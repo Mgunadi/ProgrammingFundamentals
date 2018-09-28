27d66b49bf27c21836db113edb18721ef9f55f65
Mgunadi <mgun_27@hotmail.com>
2018-09-28 13:19:59 +1000

    Update to classes

--- Comment.java
+++ Comment.java
@@ -2,11 +2,12 @@ import java.util.ArrayList;
 public class Comment {
 
 	private User user;
-	private String cmt;
-	private ArrayList<Comment> Collection = new ArrayList<Comment>();
+	private String text;
+	private ArrayList<Comment> thread = new ArrayList<Comment>();
 	
-	public Comment(User u, String cmt, ArrayList<Comment> clt) {
-		user = u;
+	public Comment(User user, String text, ArrayList<Comment> thread) {
+		
+		this.user = user;
 		this.cmt = cmt;
 		Collection = clt;
 	}
