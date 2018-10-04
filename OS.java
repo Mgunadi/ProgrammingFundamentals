dc005788d74eed5a5730912e4c314cf3683a3e54
VerityMiles <verityfmiles@gmail.com>
2018-10-04 21:41:37 +1000

    Update OS.java

--- OS.java
+++ OS.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 public class OS {
 
 	private String OSname;
@@ -17,28 +15,5 @@ public class OS {
 	public int getOSversion() {
 		return version;
 	}
-	
 
-=======
-
-public class OS {
-
-	private String OSname;
-	private int version;
-	
-	public OS(String OSname, int version) {
-		this.OSname = OSname;
-		this.version = version;
-	}
-	
-	public String getOSname() {
-		return OSname;
-	}
-	
-	public int getOSversion() {
-		return version;
-	}
-	
-	
->>>>>>> a12cc789ac90f9fd4f80805df6d4e86b29b039a7
 }
