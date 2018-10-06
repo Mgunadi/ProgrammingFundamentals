4749a260b30ac0f0f93768f74fc5654c48f36480
VerityMiles <verityfmiles@gmail.com>
2018-10-06 22:06:42 +1000

    More Verity updates

--- BalanceException.java
+++ BalanceException.java
@@ -5,34 +5,13 @@ public class BalanceException extends Exception{
 	private double balance;
 	private double amount;
 	
-	public BalanceException(double balance, double amount) {
-		super("Invalid balance " + balance);
-		this.balance = balance;
-		this.amount = amount;
-	}
-	
-	public double getBalance() {
-		return balance;
-	}
-	
-	private String reason;
-	private double maxAvailable;
-	
-	public String getReason() {
-		return reason;
-	}
-	
-	public double maxAvailable() {
-		return maxAvailable;
-	}
-	
-	public BalanceException(String reason) {
-		this.reason = reason;
-	}
-	
-	public BalanceException(String reason, double maxAvailable) {
-		this.reason = reason;
-		this.maxAvailable = maxAvailable;
-	}
-
+	public BalanceException(double balance, double amount)
+		{
+		//super("Invalid balance " + balance);
+		super("This purchase will result in a negative balance of $" + (balance - amount) +
+				"\n The purchase will not go ahead" + 
+				"\n Please choose a product that is less than $" + balance);
+		//this.balance = balance;
+		//this.amount = amount;
+		}
 }
--- User.java
+++ User.java
@@ -8,50 +8,57 @@ public class User {
 	
 	private boolean isPremium = false; 
 	private ArrayList<Content>Library = new ArrayList<Content>();
-	boolean hasEnough;
+	//boolean hasEnough;
 	double price;
 	
-	public User(String ID, String name, String phone, double balance, OS os) {
+	public User(String ID, String name, String phone, double balance, OS os)
+		{
 		UserID = ID;
 		UserName = name;
 		phoneNo = phone;
 		this.balance = balance;
 		this.os = os;
-	}
+		}
 	
-	public void becomePremium() {
-		try {
-			System.out.println("Your initial balance is: $" + balance);
-			
-			if (balance < 100) {
+	public void becomePremium()
+		{
+		try
+			{	
+			if (balance < 100)
+				{
 				throw new BalanceException(balance, 100);
 				}
 			balance = balance - 100;
 			isPremium = true;
-			System.out.println("Congratulations you are now a premium member");
+			System.out.println("Congratulations " + UserName + " you are now a premium member");
 			}
-		catch (BalanceException e) {
-			System.out.println("You do not have enough money in your account");
+		catch (BalanceException e)
+			{
+			System.out.println(e);
 			System.out.println("You are not a premium member");
-		}	
-	}
+			}	
+		}
 	
-	public void checkCompatibility(Game game) {
-		if (Objects.equals(game.getOSname(), os.getOSname()) == true) {
-			if (game.getOSversion() <= os.getOSversion()) {
+	public void checkCompatibility(Game game)
+		{
+		if (Objects.equals(game.getOSname(), os.getOSname()) == true)
+			{
+			if (game.getOSversion() <= os.getOSversion())
+				{
 				System.out.println("You have the correct operating system and version to play " + game.getName());
-			}
+				}
 			else 
 				{
 				System.out.println("You are trying to purchase a game that you do not have the correct version of the operating system");
 				System.out.println("You need to update your operating system or choose a different version of " + game.getName());
 				}
-		}
+			}
 		else 
 			System.out.println("Your operating system and the game are not compatible. Please choose another game");
-	}
+		}
 	
-	public void buyContent(Content content) {
+	public void buyContent(Content content)
+		{
 		if (content instanceof Game)
 			{
 			checkCompatibility((Game)content);
@@ -66,22 +73,21 @@ public class User {
 			price = content.getPrice() - (content.getPrice() * 0.1);
 			System.out.println("The price you get to pay is $" + price);
 			}
-		try {
-			System.out.println("Your initial balance is: $" + balance);
+		try
+			{
 			if (balance < price) {
 				throw new BalanceException(balance, price);
 			}
 			balance = balance - price;
 			content.addDownload();
 			Library.add(content);
-			System.out.println("You have successfully bought " + content.getName());
+			System.out.println(UserName + ", you have successfully bought " + content.getName());
 			System.out.println("Your balance is now: $" + balance);
 			}
-		catch (BalanceException e) {
-			System.out.println("You do not have enough money in your account");
-			System.out.println("The purchase has not been made");
-		}
-			
+		catch (BalanceException e)
+			{
+			System.out.println(e);
+			}		
 		}
 	
 	public void showContentBought()
@@ -90,10 +96,8 @@ public class User {
 			System.out.println(Library.get(i));
 		}
 	
-	public String getUserID() {
+	public String getUserID()
+		{
 		return UserID;
 		}
-
-	
-	
 }
