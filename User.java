2c9eff3999d5a40be0d83d239881127628661d30
VerityMiles <verityfmiles@gmail.com>
2018-10-14 14:09:43 +1100

    Update User.java

--- User.java
+++ User.java
@@ -3,102 +3,107 @@ import java.util.Objects;
 
 public class User {
 	
-	private String userID, userName, phoneNo;
+	private String UserID, UserName, phoneNo;
 	private double balance;
 	private OS os;
 	
 	private boolean isPremium = false; 
 	private ArrayList<Content>Library = new ArrayList<Content>();
-	boolean hasEnough;
 	double price;
 	
-	public User(String id, String name, String phone, double balance, OS os) {
-		userID = id;
-		userName = name;
+	
+	public User(String ID, String name, String phone, double balance, OS os) {
+		UserID = ID;
+		UserName = name;
 		phoneNo = phone;
 		this.balance = balance;
 		this.os = os;
 	}
 	
 	
-	public boolean enoughBalance(double amount) {
-		if ((balance - amount) > 0) {
-			hasEnough = true;
-			return hasEnough;
+	public void becomePremium() {
+		try {	
+			if (balance < 100) {
+				throw new BalanceException(balance, 100);
+			}
 			
-		} else {
-			System.out.println("You do not have enough money in your account");
-			hasEnough = false;
-			return hasEnough;
+			balance = balance - 100;
+			isPremium = true;
+			System.out.println("Congratulations " + UserName + " you are now a premium member");
+		
+		} catch (BalanceException e) {
+			System.out.println(e);
+			System.out.println("You are not a premium member");
 		}	
 	}
 	
 	
-	public void becomePremium() {
-		if (enoughBalance(100) == true) {
-			balance = balance - 100;
-			System.out.println("Your new balance is: $" + balance);
-			isPremium = true;
-			System.out.println("Congratulations you are now a premium member");
-		
-		} else {
-			System.out.println("Please add more money into your account and then try again");
-			isPremium = false;
-		}			
-	}
-	
-	
 	public void checkCompatibility(Game game) {
-		if (Objects.equals(game.getOSname(), os.getOSname()) == true) {
-			if (game.getOSversion() <= os.getOSversion()) {
+		try {
+			if (Objects.equals(game.getOSname(), os.getOSname()) == true) {
+				if (game.getOSversion() <= os.getOSversion()) {
 				System.out.println("You have the correct operating system and version to play " + game.getName());
-				
-			} else {
-				System.out.println("You are trying to purchase a game that you do not have the correct version of the operating system");
-				System.out.println("You need to update your operating system or choose a different version of " + game.getName());
 				}
-		
-		} else { 
-			System.out.println("Your operating system and the game are not compatible. Please choose another game");
+				
+				else {
+					throw new VersionException(os.getOSversion(), game.getOSversion());
+				}
+			}
+			
+			else {
+				throw new OSException(os.getOSname(), game.getOSname());
+			}
 		}
-	}
+		
+		catch (OSException e) {
+			System.out.println(e);
+		}	
+		catch (VersionException e)
+			{
+			System.out.println(e);
+			}
+		}
 	
-	
-	public void buyContent(Content content) {
-		if (content instanceof Game) {
+	public void buyContent(Content content)
+		{
+		if (content instanceof Game)
+			{
 			checkCompatibility((Game)content);
-		}
-		
+			}
 		price = content.getPrice();
-		
-		if (isPremium == true) {
+		System.out.println("The price is: $" + price);
+
+		if (isPremium == true)
+			{
+			System.out.println("You are a premium member so you get a discount");
 			price = content.getPrice() - (content.getPrice() * 0.1);
-		}
-		
-		if (enoughBalance(price) == true) {
-			//buy
+			System.out.println("The price you get to pay is $" + price);
+			}
+		try
+			{
+			if (balance < price) {
+				throw new BalanceException(balance, price);
+			}
 			balance = balance - price;
 			content.addDownload();
 			Library.add(content);
-			System.out.println("You have successfully bought " + content.getName());
+			System.out.println(UserName + ", you have successfully bought " + content.getName());
 			System.out.println("Your balance is now: $" + balance);
+			}
+		catch (BalanceException e)
+			{
+			System.out.println(e);
+			}		
 		}
-	}
 	
-	
-	public void showContentBought() {
-		for (int i = 0; i < Library.size(); i++) {
+	public void showContentBought()
+		{
+		for (int i = 0; i < Library.size(); i++)
 			System.out.println(Library.get(i).getName());
 		}
-	}
 	
-	
-	public String getUserID() {
-		return userID;
-	}
-	
-	public String getUserName() {
-		return userName;
-	}
-	
+	public String getUserID()
+		{
+		return UserID;
+		}
 }
