995a1219c9353debe27dc624f324641d33a45a81
VerityMiles <verityfmiles@gmail.com>
2018-10-14 13:13:51 +1100

    Update BalanceException.java

--- BalanceException.java
+++ BalanceException.java
@@ -1,35 +1,12 @@
-
-public class BalanceException extends Exception{
+public class BalanceException extends Exception {
 	
+	private double balance, amount;
 	
-	private double balance;
 	
 	public BalanceException(double balance, double amount) {
-		super("Invalid balance " + balance);
+		super("This purchase will result in a negative balance of $" + (balance - amount) +
+				"\n The purchase will not go ahead" + 
+				"\n Please choose a product that is less than $" + balance);
 	}
 	
-	public double getBalance() {
-		return balance;
-	}
-	
-	private String reason;
-	private double maxAvailable;
-	
-	public String getReason() {
-		return reason;
-	}
-	
-	public double maxAvailable() {
-		return maxAvailable;
-	}
-	
-	public BalanceException(String reason) {
-		this.reason = reason;
-	}
-	
-	public BalanceException(String reason, double maxAvailable) {
-		this.reason = reason;
-		this.maxAvailable = maxAvailable;
-	}
-
 }
